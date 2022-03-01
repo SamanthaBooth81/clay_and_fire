2c44444ba3df6bd1247701e5f2a618d9a2d09edc
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-03-01 13:49:31 +0000

    Initial Commit

---
+++ clay_and_fire/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for clay_and_fire project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clay_and_fire.settings')
+
+application = get_asgi_application()
