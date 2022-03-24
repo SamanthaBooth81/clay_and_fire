edef51f29ab95a27f28535b400d9170985be8fce
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-03-24 20:14:50 +0000

    start adding bootstrap toasts

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,9 @@
 """Shopping Bag Views"""
 
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Products
 
 
 def view_bag(request):
@@ -10,6 +13,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """Add to Bag View"""
+    product = Products.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     # The below checks if there is a bag variable in
@@ -20,6 +24,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
