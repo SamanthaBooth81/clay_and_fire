a2b970954b615cd10d49177598392590cc5a427c
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-04-02 16:57:07 +0000

    add checkout app admin and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,42 @@
+"""Admin setup for the checkout models"""
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """Admin setup for the OrderLineItem model
+    Tabular Inline allows admin to edit line items
+    inside the Order model"""
+    model = OrderLineItem
+    # Read Only Fields
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """Admin setup for the Order model"""
+    # Inlines creates the ability to edit models
+    # on the same page as a parent model
+    inlines = (OrderLineItemAdminInline,)
+
+    # Read Only Fields
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    # Keep the order the same as the model
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    # Show only key items
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    # Most recent orders first
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    # Overrides ready method and imports signals module
+    def ready(self):
+        import checkout.signals
