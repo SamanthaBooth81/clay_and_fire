1ed0d1f0b2d41b98405be7760521b1f1c526f6a4
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-04-10 16:21:46 +0000

    start adding Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -17,6 +17,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'STRIPE_PUBLIC_KEY': 'pk_test_51Kn1vYEKAndUjYSKJu7wWV9kizg5RxPiMii1Oji9C3z1YWj9CoGDepToNr9NELAgt9gUrKIffeuZfCnglUjdRh2Q00Zel5yVzW',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
