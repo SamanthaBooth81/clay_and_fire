4a84241d7e1e0b423050830b1321debc0f20b75b
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-04-24 13:30:41 +0000

    added profile model, view and template

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,40 @@
+"""User Profile Model"""
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class UserProfile(models.Model):
+    """User Profile Model to contain default delivery information.
+    All address fields are optional"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(
+        max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True)
+    default_county = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(
+        max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        """Return the Username"""
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """create a profile or update it if the user exists."""
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Save only, if profile exists
+    instance.userprofile.save()
