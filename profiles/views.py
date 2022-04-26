6b2c97115b13b08593e3f562421d1d99626b3a04
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-04-26 19:24:07 +0000

    added order history and save delivery info to profile

--- profiles/views.py
+++ profiles/views.py
@@ -2,11 +2,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
+from checkout.models import Order
+
 from .models import UserProfile
 from .forms import UserProfileForm
 
 
-def profile(request):
+def user_profile(request):
     """Display the user's profile"""
     profile = get_object_or_404(UserProfile, user=request.user)
 
@@ -24,6 +26,22 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """Shows the user their full order confirmation"""
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}.'))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
