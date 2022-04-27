8df3b92e283e7e267efcf56eb86a660130fda971
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-04-27 20:03:24 +0000

    ammend profile view to display error message if form incorrect

--- profiles/views.py
+++ profiles/views.py
@@ -16,9 +16,13 @@ def user_profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully')
+            messages.success(request, 'Profile updated successfully!')
+        else:
+            messages.error(
+                request, 'Update failed! Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
 
-    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
