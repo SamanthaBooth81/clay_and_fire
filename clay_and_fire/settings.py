c1e854355af402e135bdf9789787e834a7570809
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-04-28 12:33:59 +0000

    amend confirmation email subject and database setup

--- clay_and_fire/settings.py
+++ clay_and_fire/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
-    
+
     'crispy_forms',
 ]
 
@@ -130,7 +130,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
