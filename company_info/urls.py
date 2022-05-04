d5011b27681797753e2454d13d954ea1262dacd7
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-05-04 15:07:26 +0000

    add the footer company and help information pages

--- company_info/urls.py
+++ company_info/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('faq/', views.faq_page, name='faq'),
+    path('shipping-returns/', views.shipping_returns, name='shipping-returns'),
+    path('about-us/', views.about_us, name='about-us'),
 ]
--- company_info/views.py
+++ company_info/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 
 
 def faq_page(request):
-    """View to return index page"""
+    """View to return FAQ's page"""
     return render(request, 'help/faqs.html')
+
+
+def shipping_returns(request):
+    """View to return shipping and returns info page"""
+    return render(request, 'help/shipping-returns.html')
+
+
+def about_us(request):
+    """View to return shipping and returns info page"""
+    return render(request, 'company/about-us.html')
