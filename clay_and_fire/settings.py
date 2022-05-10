e0da6bf0a069c9fb75df0894bc69bcd3a9eafbe6
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-05-10 13:53:23 +0000

    amend aws settings

--- clay_and_fire/settings.py
+++ clay_and_fire/settings.py
@@ -192,7 +192,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3-website.eu-west-2.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
@@ -203,8 +203,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
-    # http://clay-and-fire.s3-website.eu-west-2.amazonaws.com
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
