1f0ffed4dc82a4c7a64fdd22a34ea438d01cdf4c
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-05-10 14:18:52 +0000

    fix static file deploy

---
+++ staticfiles/profiles/js/countryfield.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#8e9ba8');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#8e9ba8');
+    } else {
+        $(this).css('color', '#495057');
+    }
+});
