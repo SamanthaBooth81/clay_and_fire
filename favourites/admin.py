03fb3c5b179e67faf4d6bf072445c5ac15a3343b
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-07-01 13:23:14 +0000

    add admin for favourites app

--- favourites/admin.py
+++ favourites/admin.py
@@ -1,3 +1,13 @@
+""" Favourites Admin """
 from django.contrib import admin
+from .models import Favourites
 
-# Register your models here.
+
+class FavouritesAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Favourites model.
+    """
+    list_display = ('username',)
+
+
+admin.site.register(Favourites, FavouritesAdmin)
